f1a7a72dcfb5350730a798df1a83b31808e9ef57
Andriy Orehov <andriyorehov@gmail.com>
2016-10-20 20:38:08 +0300

    initialize

---
+++ apple.py
@@ -0,0 +1,4 @@
+n, k = int(input()), int(input())
+schoo = k / n
+inbucket = k % n
+print(int(schoo), '\n', inbucket)
---
+++ triangle.py
@@ -0,0 +1,2 @@
+a, b = int(input()), int(input())
+print("площадь:", (1 / 2) * a * b)
